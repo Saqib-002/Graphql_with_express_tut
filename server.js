b032a761ef3aae7ff64fc39235029b49cf1ed122
Saqib-002 <saqibshah2882@gmail.com>
2023-04-24 03:16:36 +0500

    added apollo server

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 const express=require('express');
 const path=require('path')
 const {loadFilesSync}=require('@graphql-tools/load-files')
-const {graphqlHTTP}=require('express-graphql')
-const {makeExecutableSchema}=require('@graphql-tools/schema')
+const {ApolloServer}=require('apollo-server-express')
+const {makeExecutableSchema}=require('@graphql-tools/schema');
+const { start } = require('repl');
 
 const typesArray = loadFilesSync('**/*', {
     extensions: ['graphql']
   });
 const resolversArray = loadFilesSync('**/*.resolvers.js');
-const schema=makeExecutableSchema({
-    typeDefs:typesArray,
-    resolvers:resolversArray
-})
 
+async function startApolloServer(){
+    const app=express();
+    const schema=makeExecutableSchema({
+        typeDefs:typesArray,
+        resolvers:resolversArray
+    })
+    const server=new ApolloServer({
+        schema
+    });
+    await server.start();
+    server.applyMiddleware({app,path:'/graphql'})
+    app.listen(3000,()=>{
+        console.log('listening on port 3000');
+    })
+}
 
-const app=express();
-
-app.use("/graphql",graphqlHTTP({
-    schema:schema,
-    graphiql:true
-}))
-
-app.listen(3000,()=>{
-    console.log('listening on port 3000');
-})
+startApolloServer();
